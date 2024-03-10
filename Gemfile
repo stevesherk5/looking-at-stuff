265911369fb81bf50d9f6d0a8a1a4a534d4b2aae
stevesherk5 <155704485+stevesherk5@users.noreply.github.com>
2024-03-10 18:36:14 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "3.2.2"
+
+gem "rails", "~> 7.1.3"
+gem "puma", ">= 5.0"
+gem "bcrypt", "~> 3.1.7"
+gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem "aws-sdk-s3", require: false
+
+group :development, :test do
+  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "sqlite3", "~> 1.4"
+end
+
+group :development do
+  gem "tabulo"
+  gem "web-console"
+end
+
+group :production do
+  gem "pg"
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+
+    # alternative responses to requests other than HTML
+    respond_to do |format|
+      format.html # implicitly renders posts/index.html.erb
+      format.json do
+        render :json => @posts
+      end
+    end
+  end
+
+  def new
+    @user = User.find_by({ "id" => session["user_id"] })
+  end
+
+  def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post.uploaded_image.attach(params["uploaded_image"])
+      @post["user_id"] = @user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/posts"
+  end
+
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
+end
